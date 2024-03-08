632dbc0a1ebf58c6ffa970cf59301be8ac39fd06
Sebastian Bober <139238509+Sebastian4090@users.noreply.github.com>
2024-03-08 11:40:10 +0100

    Update image.js

--- controllers/image.js
+++ controllers/image.js
@@ -1,15 +1,8 @@
 
-// const PAT = process.env.PAT;
-// const USER_ID = process.env.idUSER;
-// const APP_ID = process.env.idAPP;
-// const MODEL_ID = process.env.idMODEL;
-
-
-const PAT = '299df6884f304559ba8cc43b13de7a3d'
-const USER_ID = 'b30em380htn_dpasa';
-const APP_ID = 'faceSpotterAI';
-const MODEL_ID = 'face-detection';
-
+const PAT = process.env.PAT;
+const USER_ID = process.env.idUSER;
+const APP_ID = process.env.idAPP;
+const MODEL_ID = process.env.idMODEL;
 
 import { ClarifaiStub, grpc } from "clarifai-nodejs-grpc";
 const stub = ClarifaiStub.grpc();
